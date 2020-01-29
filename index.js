aff5f24af5f641b9b020ad9f5721814c79522819
Mark Vincent <markdeanilvicente@gmail.com>
2020-01-29 10:43:47 +0800

    update index

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 module.exports = (firstDate, secondDate) => {
-    if(!firstDate || !secondDate){
-      console.error("Firstdate & second date are empty.")
-      return undefined
-    }
-  
-    if(!firstDate){
-      console.error("Empty first date.")
-      return undefined
-    }
-  
-    if(!secondDate){
-      console.error("Empty second date.")
-      return undefined
-    }
-  
-    const getDaysDiffBetweenDates = (new Date(firstDate) - new Date(secondDate)) / (1000 * 3600 * 24);
-  
-    if(getDaysDiffBetweenDates < 0){
-      console.error("Invalid date result. Negative value.")
-      return 'Invalid date result. Negative value.'
-    }
-  
-    return getDaysDiffBetweenDates;
-  };
-  
+  if (!firstDate || !secondDate) {
+    console.error("Firstdate & second date are empty.");
+    return undefined;
+  }
+
+  if (!firstDate) {
+    console.error("Empty first date.");
+    return undefined;
+  }
+
+  if (!secondDate) {
+    console.error("Empty second date.");
+    return undefined;
+  }
+
+  const getDaysDiffBetweenDates =
+    (new Date(firstDate) - new Date(secondDate)) / (1000 * 3600 * 24);
+
+  if (getDaysDiffBetweenDates < 0) {
+    console.error("Invalid date result. Negative value.");
+    return "Invalid date result. Negative value.";
+  }
+
+  return getDaysDiffBetweenDates;
+};
